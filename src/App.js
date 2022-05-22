ef6c9feaef0ff2e1c34020e8a04664850f902796
Kushal Hiremath <hiremathkushal@outlook.com>
2022-05-22 23:33:03 +0530

    commit two

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
+import Contact from './pages/Contact';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Projects from './pages/Projects';
+//PUSHING
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar />
+      <Routes>
+      <Route path="/Home" element={<Home />} />
+        {/* <Route path="/" exact component={Home} /> */}
+        <Route path="/Services" element={<Services />} />
+        {/* <Route path="/Services" component={Services} /> */}
+        <Route path="/Contact" element={<Contact />} />
+        {/* <Route path="/Contact" component={Contact} /> */}
+        <Route path="/Projects" element={<Projects />} />
+        {/* <Route path="/Projects" component={Projects} /> */}
+       
+      </Routes>
+    </Router>
     </div>
   );
 }
